79cfd01b8293450bd7a6153b78f4e4772252694a
Mem314 <80833019+Mem314@users.noreply.github.com>
2024-09-28 19:51:43 +0200

    first commit

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import math
 import numpy as np
+from matplotlib import pyplot as plt
 
 result_list = []
+
 def condition(n):
-    if n == 1:  # Base case to terminate recursion
+
+    if n == 1:
         result_list.append(n)
         return
-    if n % 2 == 0:  # If n is even
+    if n % 2 == 0:
         n = n // 2
         result_list.append(n)
-        condition(n)  # Recursive call
-    else:  # If n is odd
+        condition(n)
+    else:
         n = n * 3 + 1
         result_list.append(n)
-        condition(n)  # Recursive call
+        condition(n)
 
-condition(6)
+
+initial_num = 39
+condition(initial_num)
 print(result_list)
+
+plt.figure()
+plt.plot(result_list, marker='o', linestyle='-', color='r')
+plt.title(f'Collatz Sequence starting from {initial_num}')
+plt.xlabel('Step')
+plt.ylabel('Value')
+plt.grid(True)
+plt.show()
